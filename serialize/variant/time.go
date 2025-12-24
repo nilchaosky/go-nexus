1ca354f234ad6c38b6cfdff4426d6b25ae5abe37
nilchaosky <1559214789@qq.com>
2025-12-24 23:40:32 +0800

    补充时间类型的Scan扫描

--- serialize/variant/time.go
+++ serialize/variant/time.go
@@ -66,6 +66,64 @@ func (t SerializeTime) Value() (driver.Value, error) {
 	return t.Time, nil
 }
 
+// Scan 从数据库驱动值扫描
+func (t *SerializeTime) Scan(value interface{}) error {
+	if value == nil {
+		t.Time = time.Time{}
+		return nil
+	}
+
+	switch v := value.(type) {
+	case time.Time:
+		t.Time = v
+		return nil
+	case []byte:
+		return t.scanFromBytes(v)
+	case string:
+		return t.scanFromString(v)
+	default:
+		return fmt.Errorf("无法扫描 %T 到 SerializeTime", value)
+	}
+}
+
+// scanFromBytes 从字节数组扫描时间
+func (t *SerializeTime) scanFromBytes(data []byte) error {
+	if len(data) == 0 {
+		t.Time = time.Time{}
+		return nil
+	}
+
+	str := string(data)
+	return t.scanFromString(str)
+}
+
+// scanFromString 从字符串扫描时间
+func (t *SerializeTime) scanFromString(str string) error {
+	if str == "" {
+		t.Time = time.Time{}
+		return nil
+	}
+
+	// 尝试多种时间格式
+	formats := []string{
+		time.DateTime,
+		time.RFC3339,
+		time.RFC3339Nano,
+		"2006-01-02 15:04:05",
+		"2006-01-02T15:04:05Z07:00",
+		"2006-01-02T15:04:05.999999999Z07:00",
+	}
+
+	for _, format := range formats {
+		if parsedTime, err := time.Parse(format, str); err == nil {
+			t.Time = parsedTime
+			return nil
+		}
+	}
+
+	return fmt.Errorf("无法解析时间格式: %s", str)
+}
+
 // DateOnly 返回DateOnly格式字符串
 func (t SerializeTime) DateOnly() string {
 	if t.Time.IsZero() {

3404c470b793f2bd5e95c60e6c15bfbb610d7468
nilchaosky <1559214789@qq.com>
2026-01-01 04:29:58 +0800

    调整model命名

--- gorm-model/sqlite/status.go
+++ gorm-model/sqlite/status.go
@@ -6,5 +6,5 @@ import (
 
 // Status 状态结构体
 type Status struct {
-	Status nexusenum.Status `json:"status" gorm:"column:status;not null;default:1;comment:状态"`
+	Value nexusenum.Status `json:"status" gorm:"column:status;not null;default:1;comment:状态"`
 }

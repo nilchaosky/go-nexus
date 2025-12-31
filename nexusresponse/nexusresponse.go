76b3ca6dce7b9fe505645cd8d64e98b63ebf4d2d
nilchaosky <1559214789@qq.com>
2026-01-01 01:29:25 +0800

    优化response模块

--- nexusresponse/nexusresponse.go
+++ nexusresponse/nexusresponse.go
@@ -10,28 +10,22 @@ const (
 )
 
 // Response 统一响应结构体
-// T 为泛型类型，表示响应数据的类型
 type Response[T any] struct {
-	Code    int    `json:"code"`    // 状态码
-	Message string `json:"message"` // 响应消息
-	Data    T      `json:"data"`    // 响应数据，使用泛型
+	Code    int    `json:"code"`
+	Message string `json:"message"`
+	Data    *T     `json:"data"`
 }
 
 // Page 分页结构体
-// T 为泛型类型，表示记录项的类型
 type Page[T any] struct {
-	Current int   `json:"current"` // 当前页码
-	Size    int   `json:"size"`    // 每页大小
-	Total   int64 `json:"total"`   // 总记录数
-	Records []T   `json:"records"` // 记录列表，使用泛型切片
+	Current int   `json:"current"`
+	Size    int   `json:"size"`
+	Total   int64 `json:"total"`
+	Records []*T  `json:"records"`
 }
 
 // Success 创建成功响应
-// data 为响应数据，使用泛型类型
-// 支持值类型和指针类型，例如：
-//   - Success(user) 返回 Response[User]
-//   - Success(&user) 返回 Response[*User]
-func Success[T any](data T) Response[T] {
+func Success[T any](data *T) Response[T] {
 	return Response[T]{
 		Code:    CodeSuccess,
 		Message: MessageSuccess,
@@ -39,21 +33,18 @@ func Success[T any](data T) Response[T] {
 	}
 }
 
-// SuccessWithNil 创建成功响应，data为nil
-// 返回Response[any]类型，适用于不需要类型约束的场景
-func SuccessWithNil() Response[any] {
-	return Response[any]{
+// SuccessWithNil 创建成功响应
+func SuccessWithNil() Response[interface{}] {
+	return Response[interface{}]{
 		Code:    CodeSuccess,
 		Message: MessageSuccess,
 		Data:    nil,
 	}
 }
 
-// SuccessWithNilData 创建成功响应，data为nil指针
-// 返回Response[*T]类型，提供类型安全的nil响应
-// 适用于需要明确数据类型但数据为空的场景
-func SuccessWithNilData[T any]() Response[*T] {
-	return Response[*T]{
+// SuccessWithNilData 创建成功响应，data为nil
+func SuccessWithNilData() Response[interface{}] {
+	return Response[interface{}]{
 		Code:    CodeSuccess,
 		Message: MessageSuccess,
 		Data:    nil,
@@ -61,9 +52,8 @@ func SuccessWithNilData[T any]() Response[*T] {
 }
 
 // SuccessPage 创建分页成功响应
-// data 为分页数据，使用泛型类型
-func SuccessPage[T any](data *Page[*T]) Response[*Page[*T]] {
-	return Response[*Page[*T]]{
+func SuccessPage[T any](data *Page[T]) Response[Page[T]] {
+	return Response[Page[T]]{
 		Code:    CodeSuccess,
 		Message: MessageSuccess,
 		Data:    data,
@@ -71,10 +61,8 @@ func SuccessPage[T any](data *Page[*T]) Response[*Page[*T]] {
 }
 
 // Error 创建错误响应
-// message 为错误消息，data为nil
-// 返回Response[any]类型，适用于不需要类型约束的场景
-func Error(message string) Response[any] {
-	return Response[any]{
+func Error(message string) Response[interface{}] {
+	return Response[interface{}]{
 		Code:    CodeError,
 		Message: message,
 		Data:    nil,
@@ -82,30 +70,8 @@ func Error(message string) Response[any] {
 }
 
 // ErrorWithCode 创建自定义错误码的错误响应
-// code 为自定义错误码，message 为错误消息，data为nil
-// 返回Response[any]类型，适用于不需要类型约束的场景
-func ErrorWithCode(code int, message string) Response[any] {
-	return Response[any]{
-		Code:    code,
-		Message: message,
-		Data:    nil,
-	}
-}
-
-// ErrorWithNilData 创建错误响应，data为nil指针
-// message 为错误消息，返回Response[*T]类型，提供类型安全的nil响应
-func ErrorWithNilData[T any](message string) Response[*T] {
-	return Response[*T]{
-		Code:    CodeError,
-		Message: message,
-		Data:    nil,
-	}
-}
-
-// ErrorWithCodeAndNilData 创建自定义错误码的错误响应，data为nil指针
-// code 为自定义错误码，message 为错误消息，返回Response[*T]类型，提供类型安全的nil响应
-func ErrorWithCodeAndNilData[T any](code int, message string) Response[*T] {
-	return Response[*T]{
+func ErrorWithCode(code int, message string) Response[interface{}] {
+	return Response[interface{}]{
 		Code:    code,
 		Message: message,
 		Data:    nil,
